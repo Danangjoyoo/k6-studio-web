1a5d30cf681e5ac739ae1a255b293f74731a3ada
danjoyboy <danangjoyo.agus@moneyforward.co.jp>
2026-06-25 20:09:07 +0900

    test: ignore next build output in jest

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,7 @@ const config: Config = {
   preset: "ts-jest",
   testEnvironment: "node",
   testPathIgnorePatterns: ["/node_modules/", "/e2e/"],
+  modulePathIgnorePatterns: ["<rootDir>/.next/"],
   transform: {
     "^.+\\.tsx?$": [
       "ts-jest",
